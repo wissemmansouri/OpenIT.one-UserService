bc866064a3635211080f770a57b810208fa1f002
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-09 12:52:24 +0200

    Create service.go

---
+++ service/service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"github.com/wissemmansouri/OpenIT.one-Common/external"
+	"gorm.io/gorm"
+)
+
+var MyService Repository
+
+type Repository interface {
+	Gateway() external.ManagementService
+	User() UserService
+	Event() EventService
+}
+
+func NewService(db *gorm.DB, RuntimePath string) Repository {
+
+	gatewayManagement, err := external.NewManagementService(RuntimePath)
+	if err != nil {
+		panic(err)
+	}
+
+	return &store{
+		gateway: gatewayManagement,
+		user:    NewUserService(db),
+		event:   NewEventService(db),
+	}
+}
+
+type store struct {
+	gateway external.ManagementService
+	user    UserService
+	event   EventService
+}
+
+func (c *store) Event() EventService {
+	return c.event
+}
+func (c *store) Gateway() external.ManagementService {
+	return c.gateway
+}
+
+func (c *store) User() UserService {
+	return c.user
+}
